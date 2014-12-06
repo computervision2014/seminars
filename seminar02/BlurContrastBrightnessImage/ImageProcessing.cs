331c6bd311300fa92e16469816ee351e3320fadd
Alena Sibirtseva <elsibirtseva@gmail.com>
2014-12-06 14:56:20 +0300

    seminar 2 source (blur, brightness, contrast)

---
+++ seminar02/BlurContrastBrightnessImage/ImageProcessing.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlurContrastBrightnessImage
+{
+    class ImageProcessing
+    {
+        /* blurs image with Gaussian kernel
+         this kernel looks like: 
+         | 1/16f, 1/8f, 1/16f, |
+         | 1/8f, 1/4f, 1/8f,   |
+         | 1/16f, 1/8f, 1/16f, |
+        */
+
+        public byte[] setGaussianBlur(byte[] originalImage, int coef)
+        {
+
+            // TODO: create kernel and new image
+            // TODO: convolve it with Gaussian kernel
+
+            return null;
+        }
+
+        // changes birghness of an image
+        public byte[] setBrightness(byte[] originalImage, int coef)
+        {
+            // TODO: set brightness to the image
+            // it looks like: newPixel = oldPixel + coef
+            // don't forget about pixel [0, 255]
+            // TODO: check if the value is less than 0 -> make pixel = 0
+            // TODO: check if the value is more than 255 -> make pixel = 255
+            // else everything is fine
+
+            return null;
+        }
+
+        // changes contrast of an image
+        public byte[] setContrast(byte[] originalImage, int coef)
+        {
+            // TODO: create contrastLevel = ((100.0 + coef)/100.0)^2
+            // TODO: all pixels should have adjusted contrast, according to the formula
+            // newPixel = (((oldPixel/255.0 - 0.5) * contastLevel) + 0.5) * 255.0
+            // don't forget to check all the values!
+
+            return null;
+        }
+
+        // checks if the value is in the correct interval [0, 255] 
+        private byte checkPixelValue(int pixel)
+        {
+            // TODO: check pixel value and return 0, 255 or pixel itself 
+            return (byte)pixel;
+        }
+    }
+}
---
+++ seminar02/BlurContrastBrightnessImage/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlurContrastBrightnessImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage originalImage;
+        private byte[] originalImageBytes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // open file from the finder
+        private void openFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg)|*.jpg; *.jpeg";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                showImage(openFileDialog.FileName);
+                slidersGrid.IsEnabled = true;
+
+            }
+        }
+        // show image on the window
+        private void showImage(string filename)
+        {
+            originalImage = ImageConvertor.FilenameToImage(filename);
+            originalImageBytes = ImageConvertor.ImageToByteArray(filename);
+            originalPanel.Source = originalImage;
+        }
+
+        // event on slider changed value
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+	        int value = (int)slider.Value;
+            string sliderName = slider.Name;
+            ImageProcessing process = new ImageProcessing();
+            byte[] processedImageBytes;
+            switch (sliderName[0])
+            {
+                // if the slider is "Gaussian BLur"
+                case 'g':
+                    processedImageBytes = process.setGaussianBlur(originalImageBytes, value);
+                    grayscalePanel.Source = ImageConvertor.ByteArrayToImage(processedImageBytes, originalImage.PixelWidth, originalImage.PixelHeight, 4);
+                    break;
+
+                // if the sliber is "Brightness"
+                case 'b':
+                    processedImageBytes = process.setBrightness(originalImageBytes, value-50);
+                    grayscalePanel.Source = ImageConvertor.ByteArrayToImage(processedImageBytes, originalImage.PixelWidth, originalImage.PixelHeight, 4);
+                    break;
+
+                // if the slider is "Contrast"
+                case 'c':
+                    processedImageBytes = process.setContrast(originalImageBytes, value-100);
+                    grayscalePanel.Source = ImageConvertor.ByteArrayToImage(processedImageBytes, originalImage.PixelWidth, originalImage.PixelHeight, 4);
+                    break;
+
+                // if smth stupid happend
+                default:
+                    break;
+            }
+        }
+
+
+    }
+}
